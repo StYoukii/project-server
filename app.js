905685e1314716a8a7009c8d050c0b5ebb0889bc
StYoukii <gcottin26@gmail.com>
2018-07-13 20:19:28 +0200

    Mise en place du serveur Express et de MongoDB

--- app.js
+++ app.js
@@ -1,4 +1,42 @@
 let express = require('express');
-let http = require('http');
 let path = require('path');
-let util = require('util');
+let favicon = require('serve-favicon');
+let logger = require('morgan');
+let bodyParser = require('body-parser');
+let mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+
+let user = require('./routes/user');
+let app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({'extended':'false'}));
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use('/users', express.static(path.join(__dirname, 'dist')));
+app.use('/user', user);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    let err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
+
+
+mongoose.connect('mongodb://StYoukii:gekdnjcd26@ds121871.mlab.com:21871/project', { useNewUrlParser: true, promiseLibrary: require('bluebird') })
+    .then(() =>  console.log('connection succesful'))
+    .catch((err) => console.error(err));
